d012723b94bcb448d58f2546b7d8f41974dcb5a3
jch6637 <jch6637@naver.com>
2018-07-26 01:02:14 -0700

    pcap_test

--- main.cpp
+++ main.cpp
@@ -1,107 +1,94 @@
 #include <pcap.h>
 #include <stdio.h>
+#include <stdint.h>
+#include <net/ethernet.h>
+#include <netinet/ip.h>
+#include <netinet/tcp.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
-void Print_MAC_Address(const u_char *packet)
+int Print_MAC_Address(const u_char *packet)
 {
-	const u_char *p = NULL;
+	struct ether_header* ethernet = (struct ether_header *)packet;  
 	int i;
-
-	p = packet;
 
 	printf("Source MAC Address      : ");
 
-	for( i = 0; i < 6; i++)
+	for (i = 0; i < 6; i++)
 	{
-		if(i < 5)
-			printf("%02x:", *(packet + i + 6) );
+		if (i < 5)
+			printf("%02x:", ethernet->ether_shost[i]);
 		else
-			printf("%02x", *(packet + i + 6) );
+			printf("%02x", ethernet->ether_shost[i]);
 	}
 
 	printf("\n");
 
 	printf("Destination MAC Address : ");
 
-	for( i = 0;  i < 6; i++)
+	for (i = 0; i < 6; i++)
 	{
-		if(i < 5)
-			printf("%02x:",*(packet + i) );
+		if (i < 5)
+			printf("%02x:", ethernet->ether_dhost[i]);
 		else
-			printf("%02x", *(packet + i) );
+			printf("%02x", ethernet->ether_dhost[i]);
 	}
 
 	printf("\n");
+	if(ntohs(ethernet->ether_type) == ETHERTYPE_IP)
+		return 1;
+
+	return 0;
 }
 
-void Print_IP_Address(const u_char *packet)
+int Print_IP_Address(const u_char *packet)
 {
-	const u_char *p = NULL;
-	int i;
+	struct iphdr *ip = (struct iphdr *)packet;
 	
-	p = packet;
-	printf("Source IP Address       : ");
+	printf("Source IP Address       : %s\n", inet_ntoa(*(struct in_addr *)&ip->saddr));
+	printf("Destination IP Address  : %s\n", inet_ntoa(*(struct in_addr *)&ip->daddr));
 	
-	for( i = 0; i < 4; i++)
-	{
-		if(i < 3)
-			printf("%d.", *(packet + i + 14));
-		else
-			printf("%d", *(packet + i + 14));
-	}
+	if(ip->protocol == 0x06)
+		return 1;
 
-	printf("\n");
-	
-	printf("Destination IP Address  : ");
-	for( i = 0 ; i < 4; i++)
-	{
-		if(i < 3)
-			printf("%d.", *(packet + i + 18) );
-		else
-			printf("%d", *(packet + i + 18) );
-	}	
-
-	printf("\n");
+	return 0;
 }
 
 void Print_Port(const u_char *packet)
 {
-	const u_char *p = NULL;
-	unsigned short result;
-	
-	p = packet;
+	struct tcphdr *tcp = (struct tcphdr *)packet;
 
-	result = *p * 0x100;
-	result += *(p + 1);
-	printf("Source Port      : %d\n", result);
-
-	result = *(p + 2) * 0x100;
-	result += *(p + 3);
-	printf("Destination Port : %d\n", result);
+	printf("Source Port      : %d\n", ntohs(tcp->source));
+	printf("Destination Port : %d\n", ntohs(tcp->dest));
 }
 
 void Hexdump(const u_char *packet, struct pcap_pkthdr *header)
 {
-	int i;
-	for(i = 1; i <= 28 + ( *(packet + 14) % 0x40 ) ; i++)
-        {
-                printf("%02x ", *packet);
-                if(i % 8 == 0) printf("  ");
-                if(i % 16 == 0) printf("\n");
-                packet++;
-        }
-        
-        printf("\n");   
-        printf("***********************************************\n");
+	int i, total;
+	struct iphdr *ip = (struct iphdr *)(packet + sizeof(struct ether_header));
+	struct tcphdr *tcp = (struct tcphdr *)(packet + sizeof(struct ether_header) + sizeof(struct iphdr));
+
+	for (i = 1; i <=  56 ; i++)
+	{
+		printf("%02x ", *packet);
+		if (i % 8 == 0) printf("  ");
+		if (i % 16 == 0) printf("\n");
+		packet++;
+	}
+
+	printf("\n");
+	printf("***********************************************\n");
 
 }
 
-void usage(){
+void usage() {
 	printf("syntax: pcap_test <interface>\n");
 	printf("sample: pcap_test wlan0\n");
 }
 
-int main(int argc, char *argv[]){
-	if(argc != 2){
+int main(int argc, char *argv[]) {
+	if (argc != 2) {
 		usage();
 		return -1;
 	}
@@ -109,32 +96,33 @@ int main(int argc, char *argv[]){
 	char *dev = argv[1];
 	char errbuf[PCAP_ERRBUF_SIZE];
 	int res, cnt = 0;
+	int ip, tcp = 0;
 	pcap_t *handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
-	
-	if(handle == NULL){
+
+	if (handle == NULL) {
 		fprintf(stderr, "Couldn't open device %s : %s\n", dev, errbuf);
 		return -1;
 	}
-	
-	while(true){
-	struct pcap_pkthdr *header;
-	const u_char *packet, *p = NULL;
 
-	res = pcap_next_ex(handle, &header, &packet);
-	if(res == 0) continue;
-	if(res == -1 || res == -2) break;	
+	while (true) {
+		struct pcap_pkthdr *header;
+		const u_char *packet, *p = NULL;
 
-	printf("\n[*]Capture the Packet! / Index : %d\n", ++cnt);
-	printf("[*]Packet pointer : %p\n", packet);
-	printf("[*]Packet bytes : %u\n", header->caplen);	
-	printf("***********************************************\n");
-	Hexdump(packet, header);	
-	Print_MAC_Address(packet);
-	if(*(packet + 12) == 0x08 && *(packet + 13) == 0x00)	
-		Print_IP_Address(packet + 12);
-	if(*(packet + 23) ==0x06)
-		Print_Port(packet + 34);
-	printf("***********************************************\n\n");
+		res = pcap_next_ex(handle, &header, &packet);
+		if (res == 0) continue;
+		if (res == -1 || res == -2) break;
+
+		printf("\n[*]Capture the Packet! / Index : %d\n", ++cnt);
+		printf("[*]Packet pointer : %p\n", packet);
+		printf("[*]Packet bytes : %u\n", header->caplen);
+		printf("***********************************************\n");
+		Hexdump(packet, header);
+		ip = Print_MAC_Address(packet);
+		if (ip)
+			tcp = Print_IP_Address(packet + sizeof(struct ether_header));
+		if (tcp)
+			Print_Port(packet + sizeof(struct ether_header) + sizeof(struct iphdr));
+		printf("***********************************************\n\n");
 
 	}
 	pcap_close(handle);
